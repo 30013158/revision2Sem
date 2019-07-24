a104ea6baa469d82631a07394d78a2c3ae3b3656
Rajveer Kaur <30013158@student.toiohomai.ac.nz>
2019-07-24 16:52:29 +1200

    first

---
+++ Sem2/Sem2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Rajveer Kaur");
+
+            Console.Write("Enter number 1: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter number 2: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Sum(num1, num2));
+            Console.WriteLine($"The division of the two numbers is :{Div(num1, num2)}");
+            Console.WriteLine(Multiply(num1, num2));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter a number to view its table:");
+            int input = int.Parse(Console.ReadLine());
+
+            /*using for loop
+            for(int i=1; i<11; i++)
+            { Console.WriteLine($"{input} x {i} = {input * i}"); }*/
+
+            //using do while loop
+            int counter = 1;
+            do {
+                Console.WriteLine($"{input} x {counter} = {input * counter}");
+                counter++;
+            }
+            while (counter < 11);
+
+            Console.WriteLine();
+
+            Console.Write("Enter the radius of the circle: ");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The Perimeter of circle is: {Perimeter(radius)}");
+            Console.WriteLine($"The Area of circle is: {Area(radius)}");
+
+
+
+
+            Console.ReadLine();
+        }
+        static string Sum(double num1, double num2)
+        { return $" {num1} + {num2} =  {num1 + num2} ";}
+
+        static double Div(double num1, double num2)
+        { return num1/num2; }
+
+        static string Multiply(double num1, double num2)
+        { return $" {num1} x {num2} =  {num1 * num2} "; }
+
+        static double Perimeter(double radius)
+        { return Math.PI * 2 * radius; }
+
+        static double Area(double radius)
+        { return Math.PI * radius * radius; }
+
+    }
+}
---
+++ class1/class1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class1
+{
+    class Program
+    {
+        
+       
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Animal A1 = new Animal();
+                Console.WriteLine("enter the animal type:");
+                A1.Type = Console.ReadLine();
+                Console.WriteLine("enter the animal name:");
+                A1.Name = Console.ReadLine();
+                Console.WriteLine("enter the animal age:");
+                A1.Age = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter the animal ID:");
+                A1.ID = int.Parse(Console.ReadLine());
+                Console.WriteLine(A1.Display());
+                animals[i] = A1;
+            }
+        }
+    }
+    class  Animal
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ID { get; set; }
+         
+        public string Display()
+        {
+            return $"....Animal Details...\nType= {Type}\n Name= {Name}\n Age= {Age}\n ID = {ID}"; 
+             
+            }
+         
+        
+    }
+}
